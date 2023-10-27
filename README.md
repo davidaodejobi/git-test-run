e12ae871a07401665f0a408ec98f54b858b7bbbe
davidaodejobi <talk2engineerdavid@gmail.com>
2023-10-27 15:32:05 +0100

    Initial work in progress

---
+++ README.md
@@ -0,0 +1,3 @@
+# git_test_runner
+
+A new Flutter project.
---
+++ android/app/src/main/kotlin/com/testruntforgit/git_test_runner/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.testruntforgit.git_test_runner
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
